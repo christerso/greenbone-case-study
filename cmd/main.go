6ec2e62e2ed953de8bc1958463d0084af674ea30
Christer Soederlund <christer.soderlund@gmail.com>
2025-07-02 17:59:41 +0200

    Added graceful shutdown.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"greenbone-computer-inventory/internal/api"
 	"greenbone-computer-inventory/internal/config"
 	"greenbone-computer-inventory/internal/database"
@@ -8,7 +10,11 @@ import (
 	"greenbone-computer-inventory/internal/repository"
 	"greenbone-computer-inventory/pkg/logger"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -40,9 +46,29 @@ func main() {
 	router := gin.Default()
 	api.SetupRoutes(router, handler, db)
 
-	log.Info("Server starting", slog.String("port", cfg.Port))
-	err = router.Run(":" + cfg.Port)
-	if err != nil {
-		return
+	srv := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: router,
 	}
+
+	go func() {
+		log.Info("Server starting", slog.String("port", cfg.Port))
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Error("Server failed to start", slog.String("error", err.Error()))
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("Server forced to shutdown", slog.String("error", err.Error()))
+	}
+
+	log.Info("Server exited")
 }
